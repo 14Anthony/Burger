ebb0f160d0c9269b0de8b9712f6bf546a49ccd6b
Anthony Belcastro <belcastt@live.com>
2020-08-27 17:56:45 -0700

    folder and directory set up complete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,4 @@
 const express = require("express");
 
 // Import the model (cat.js) to use its database functions.
-const cat = require("../models/burger.js");
+const burger = require("../models/burger.js");
