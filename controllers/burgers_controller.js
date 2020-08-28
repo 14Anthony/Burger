86b8603de9676bd72a627560af1bcc15012027fd
Anthony Belcastro <belcastt@live.com>
2020-08-27 17:43:48 -0700

    importing the burger.js file, and express into burgers_controllers.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,5 @@
+// Import the express package per the readme
+const express = require("express");
+
+// Import the model (cat.js) to use its database functions.
+const cat = require("../models/burger.js");
