28b44f16279b5b4adbcf9ebec1ba85fe93976493
Anthony Belcastro <belcastt@live.com>
2020-08-28 09:37:04 -0700

    burger table created

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,16 @@
-  
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE Table
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+      burgerName VARCHAR
+    (30),
+      devoured BOOLEAN DEFAULT true,
+      PRIMARY KEY
+    (id);
+  )
