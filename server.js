7c54f0be18662cdf38807ce186f0a40cb989bcc6
Anthony Belcastro <belcastt@live.com>
2020-08-27 17:25:04 -0700

    deletion of the mysql code and relocating it layered into the Connnection.js code within the config folder

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 const express = require("express");
-// Set up MySQL connection.
-const mysql = require("mysql");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
 
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+
 
 app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
