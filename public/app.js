e110bf72fc4fc58470750d9d1ab34397d5133278
Anthony Belcastro <belcastt@live.com>
2020-08-28 21:39:48 -0700

    more psuedo code, from thursdays 16 again.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,30 @@
 $(document).ready(function () {
     /// check for the weather application, I need to grab the entire form on submit...???  the new creation of the burger
+    // $(".create-form").on("submit", function(event) {
+    //     // Make sure to preventDefault on a submit event.
+    //     event.preventDefault();
+
+    //     const newCat = {
+    //       name: $("#ca").val().trim(),
+    //       sleepy: $("[name=sleepy]:checked").val().trim()
+    //     };
+
     /// I need to prevent the default refresh
     ///I need move that burger into an object, with its name and if it was eaten or devoured??? naming
     //I need to post that to the db, in an ajax call 
+    // Send the POST request.
+    //      $.ajax("/api/cats", {
+    //         type: "POST",
+    //         data: newCat
+    //       }).then(
+    //         function() {
+    //           console.log("created new cat");
+    //           // Reload the page to get the updated list
+    //           location.reload();
+    //         }
+    //       );
+    //     });
+    //   });
     // I need to create a button and  place it and the burger information on the page on the page.
     // then I need the put routing using ajax to a new devoured status
     // Send the PUT request.
@@ -18,6 +40,6 @@ $(document).ready(function () {
     //   );
     // });
     //I need to be able to delet the data and reload everything clean.
-    // cats doesn't seem to work here.
+
 
 })
