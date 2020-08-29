c94c853e45cc8ddddc202f3f3ebc72b1db0943cf
Anthony Belcastro <belcastt@live.com>
2020-08-28 21:35:30 -0700

    psuedo coding the app.js, pulling from Thursdays class

---
+++ public/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    /// check for the weather application, I need to grab the entire form on submit...???  the new creation of the burger
+    /// I need to prevent the default refresh
+    ///I need move that burger into an object, with its name and if it was eaten or devoured??? naming
+    //I need to post that to the db, in an ajax call 
+    // I need to create a button and  place it and the burger information on the page on the page.
+    // then I need the put routing using ajax to a new devoured status
+    // Send the PUT request.
+    // $.ajax("/api/cats/" + id, {
+    //     type: "PUT",
+    //     data: newSleepState
+    //   }).then(
+    //     function() {
+    //       console.log("changed sleep to", newSleep);
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+    //I need to be able to delet the data and reload everything clean.
+    // cats doesn't seem to work here.
+
+})
