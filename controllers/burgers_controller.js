74b7043f8efb21124a0c9fcd46e58a95b6a6c506
Anthony Belcastro <belcastt@live.com>
2020-08-28 20:04:09 -0700

    post route, and creation and sending a new Id

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,17 @@
 // Import the express package per the readme
+
 const express = require("express");
+
 //Maximillian, router use.
+
 const router = express.Router();
+
 // Import the model like cats to use its database functions.
+
 const burger = require("../models/burger.js");
+
 // Create all our routes and set up logic within those routes where required.
+
 router.get("/", function (req, res) {
     burger.all(function (data) {
         const hbsObject = {
@@ -12,5 +19,16 @@ router.get("/", function (req, res) {
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
+    });
+});
+
+//post route, burgerName and Devoured.
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(["burgerName", "devoured"], [req.body.burgerName, req.body.dev], function (result) {
+
+        // Send back the ID of the new quote
+
+        res.json({ id: result.insertId });
     });
 });
