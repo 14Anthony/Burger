1c95c965ee4fe7de3c3ac3403f64bae581dada50
Anthony Belcastro <belcastt@live.com>
2020-08-28 21:57:49 -0700

    creating variable within the models

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,11 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
+
+const burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+}
