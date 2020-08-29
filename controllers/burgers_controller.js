bc4d0b251c7f33e8decc53cfb51f1d4ca91e681c
Anthony Belcastro <belcastt@live.com>
2020-08-28 19:58:23 -0700

    using 16 as a structure creating the routing structures

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,16 @@
 // Import the express package per the readme
 const express = require("express");
-
-// Import the model (cat.js) to use its database functions.
+//Maximillian, router use.
+const router = express.Router();
+// Import the model like cats to use its database functions.
 const burger = require("../models/burger.js");
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        const hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
