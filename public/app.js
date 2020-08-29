8494504badd4acf940ec457ac885f79c7e47250b
Anthony Belcastro <belcastt@live.com>
2020-08-29 09:15:51 -0700

    reworking of previous mvc workings

--- public/app.js
+++ public/app.js
@@ -10,44 +10,44 @@ $(document).ready(function () {
         };
 
 
-        $(".btn").on("click", function (event) {
-            const id = $(this).data("id");
-            const newStage = {
-                devoured: true
-            }
 
-            // then I need the put routing using ajax to a new devoured status, 
-            // Send the PUT request.
-            $.ajax("/api/burger/" + id, {
-                type: "PUT",
-                data: newBurger
-            }).then(
-                function () {
-                    console.log("changed stage to", newStage);
-                    // Reload the page to get the updated list
-                    location.reload();
-                }
-            );
-        });
+        // /I need move that burger into an object, with its name and if it was eaten or devoured??? naming
+        // I need to post that to the db, in an ajax call 
+        // Send the POST request.
+        $.ajax("/api/cats", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new Burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
+$(".btn").on("click", function (event) {
+    const id = $(this).data("id");
+    const newStage = {
+        devoured: true
+    }
+
+    // then I need the put routing using ajax to a new devoured status, 
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newBurger
+    }).then(
+        function () {
+            console.log("changed stage to", newStage);
+            // Reload the page to get the updated list
+            location.reload();
+        }
+    );
+});
 
 
     })
-
-    ///I need move that burger into an object, with its name and if it was eaten or devoured??? naming
-    //I need to post that to the db, in an ajax call 
-    // Send the POST request.
-    //      $.ajax("/api/cats", {
-    //         type: "POST",
-    //         data: newCat
-    //       }).then(
-    //         function() {
-    //           console.log("created new cat");
-    //           // Reload the page to get the updated list
-    //           location.reload();
-    //         }
-    //       );
-    //     });
-    //   });
     // I need to create a button and  place it and the burger information on the page on the page.
     //I need to be able to delet the data and reload everything clean.
 
