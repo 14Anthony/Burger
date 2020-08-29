dd755429e6fa87cf8287e9dc8771637d602b4fae
Anthony Belcastro <belcastt@live.com>
2020-08-28 20:12:45 -0700

    syntax change, and put route added, staging burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,10 +25,31 @@ router.get("/", function (req, res) {
 //post route, burgerName and Devoured.
 
 router.post("/api/burgers", function (req, res) {
-    burger.create(["burgerName", "devoured"], [req.body.burgerName, req.body.dev], function (result) {
+    burger.create(["burgerName", "devoured"], [req.body.burgerName, req.body.devoured], function (result) {
 
         // Send back the ID of the new quote
 
         res.json({ id: result.insertId });
     });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    const stage = "id = " + req.params.id;
+
+    console.log("staged", stage);
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        stage,
+        function (result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
 });
