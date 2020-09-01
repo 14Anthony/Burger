ad5d2ba71dca9f79565c6262a6944f24b509b850
Anthony Belcastro <belcastt@live.com>
2020-08-31 19:41:27 -0700

    reformatting the html, adding class names to buttons.  cleaning up the syntax

--- public/app.js
+++ public/app.js
@@ -14,42 +14,44 @@ $(document).ready(function () {
         // /I need move that burger into an object, with its name and if it was eaten or devoured??? naming
         // I need to post that to the db, in an ajax call 
         // Send the POST request.
-        $.ajax("/api/cats", {
+        $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(
             function () {
-                console.log("created new Burger");
+                // console.log("created new Burger");
                 // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
-});
-//I need a neww naming structure in the html .....?  too many btn.s
+    //I need a neww naming structure in the html .....?  too many btn.s
 
-$(".btn").on("click", function (event) {
-    const id = $(this).data("id");
-    const newStage = {
-        devoured: true
-    }
-
-    // then I need the put routing using ajax to a new devoured status, 
-    // Send the PUT request.
-    $.ajax("/api/burger/" + id, {
-        type: "PUT",
-        data: newBurger
-    }).then(
-        function () {
-            console.log("changed stage to", newStage);
-            // Reload the page to get the updated list
-            location.reload();
+    $(".btnD").on("click", function (event) {
+        const id = $(this).data("id");
+        const newStage = {
+            devoured: true
         }
-    );
+
+        // then I need the put routing using ajax to a new devoured status, 
+        // Send the PUT request.
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newBurger
+        }).then(
+            function () {
+                // console.log("changed stage to", newStage);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
 });
 
 
 
-    //I need to be able to delet the data and reload everything clean. /// I need to create a new button class for this to work.??????I need an ajax delete that does clash with the put
+    //I need to be able to delete the data and reload everything clean. /// I need to create a new button class for this to work.??????I need an ajax delete that does clash with the put
 
 
