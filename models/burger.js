dc4ffdb56928380dcbe507b00872466100455ae7
Anthony Belcastro <belcastt@live.com>
2020-08-31 21:57:14 -0700

    I went too far with becky , I dont need a delete function

--- models/burger.js
+++ models/burger.js
@@ -2,29 +2,25 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    all: function (cb) {
+    Select: function (cb) {
         orm.all("burgers", function (res) {
             cb(res);
         });
     },
     // the variables cols and vals are arrays
-    create: function (cols, vals, cb) {
+    insertOne: function (cols, vals, cb) {
         orm.create("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
-    update: function (objColVals, stage, cb) {
+    updateOne: function (objColVals, stage, cb) {
         orm.update("burgers", objColVals, stage, function (res) {
             cb(res);
 
         });
 
     },
-    delete: function (stage, cb) {
-        orm.delete("burgers", stage, function (res) {
-            cb(res);
-        });
-    }
+
 };
 
 module.exports = burger;
