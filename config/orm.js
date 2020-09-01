bbeca0eab58a6551d9b74cd782e9548d81c7bba6
Anthony Belcastro <belcastt@live.com>
2020-08-31 22:01:00 -0700

    all thats reference to delete, is not needed per readme

--- config/orm.js
+++ config/orm.js
@@ -65,13 +65,13 @@ var orm = {
         });
     },
     // An example of objColVals would be {name: panther, sleepy: true}
-    update: function (table, objColVals, condition, cb) {
+    update: function (table, objColVals, stage, cb) {
         var queryString = "UPDATE " + table;
 
         queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
-        queryString += condition;
+        queryString += stage;
 
         console.log(queryString);
         connection.query(queryString, function (err, result) {
@@ -82,19 +82,7 @@ var orm = {
             cb(result);
         });
     },
-    delete: function (table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
-        queryString += " WHERE ";
-        queryString += condition;
 
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
-    }
 };
 
 // Export the orm object for the model (burger.js).
