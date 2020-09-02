b52bd8747cfbd821c8dc3782b89d4d8eaad7a4d1
Anthony Belcastro <belcastt@live.com>
2020-09-01 17:41:12 -0700

    one offs I forgot the els, and added information to a console.log to check for req body

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,7 +15,7 @@ const burger = require("../models/burger.js");
 router.get("/", function (req, res, next) {
     burger.selectAll(function (data) {
         const hbsObject = {
-            burger: data
+            burgers: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
@@ -36,7 +36,7 @@ router.post("/api/burgers", function (req, res, next) {
 router.put("/api/burgers/:id", function (req, res, next) {
     const stage = "id = " + req.params.id;
 
-    console.log("staged", stage);
+    console.log("staged", stage, req.body);
 
     burger.updateOne(
         {
@@ -44,12 +44,12 @@ router.put("/api/burgers/:id", function (req, res, next) {
         },
         stage,
         function (result) {
-            if (result.changedRows === 0) {
+            if (result.changedRows == 0) {
                 // If no rows were changed, then the ID must not exist, so 404
                 return res.status(404).end();
+            } else {
+                res.status(200).end();
             }
-            res.status(200).end();
-
         }
     );
 });
