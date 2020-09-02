018b210250dfd84988528dbefeba08bbb874bc5f
Anthony Belcastro <belcastt@live.com>
2020-09-01 17:19:09 -0700

    refactoring the syntax on a the name to burgerName correlation with the sql

---
+++ public/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    /// check for the weather application, I need to grab the entire form on submit...???  the new creation of the burger
+    $(".create-form").on("submit", function (event) {
+
+        event.preventDefault();
+
+        const newBurger = {
+            burgerName: $("#ca").val().trim(),
+            devoured: $("[burgerName=devoured]:checked").val().trim()
+        };
+
+
+
+        // /I need move that burger into an object, with its name and if it was eaten or devoured??? naming
+        // I need to post that to the db, in an ajax call 
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                // console.log("created new Burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+    //I need a neww naming structure in the html .....?  too many btn.s
+
+    $(".btnD").on("click", function (event) {
+        const id = $(this).data("id");
+        const newStage = {
+            devoured: true
+        }
+
+        // then I need the put routing using ajax to a new devoured status, 
+        // Send the PUT request.
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newBurger
+        }).then(
+            function () {
+                // console.log("changed stage to", newStage);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
+
+
+//const pastSch = $("<button>").appendTo('#searchHistory');
+// const pastS = $('<button>')
+// pastS.addClass('thePast')
+// pastS.text(response.name)
+// $('#searchHistory').append(pastS)
